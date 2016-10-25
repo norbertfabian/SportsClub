b270d172bff7d1210d6bf421ebf2b4fd0247191b
Šulgan Marián <marian.sulgan@gmail.com>
2016-10-25 18:03:20 +0200

    fixed pom.xml and respective imports - missing javax.validation dependency

--- persistance/src/main/java/cz/muni/fi/pa165/sportsclub/entity/TeamManager.java
+++ persistance/src/main/java/cz/muni/fi/pa165/sportsclub/entity/TeamManager.java
@@ -1,12 +1,12 @@
 package cz.muni.fi.pa165.sportsclub.entity;
 
-import com.sun.istack.internal.NotNull;
 import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.validation.constraints.NotNull;
 
 /**
  * Created by MarianSulgan on 25.10.16.
