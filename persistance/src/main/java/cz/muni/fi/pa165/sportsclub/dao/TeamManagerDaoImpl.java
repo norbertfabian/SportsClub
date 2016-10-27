21c4e58cbe7b99ac677aba006a6f396bf8115b2b
Šulgan Marián <marian.sulgan@gmail.com>
2016-10-27 18:32:58 +0200

    added User entity, TeamManager fiels fix

--- persistance/src/main/java/cz/muni/fi/pa165/sportsclub/dao/TeamManagerDaoImpl.java
+++ persistance/src/main/java/cz/muni/fi/pa165/sportsclub/dao/TeamManagerDaoImpl.java
@@ -8,11 +8,15 @@ package cz.muni.fi.pa165.sportsclub.dao;
 import cz.muni.fi.pa165.sportsclub.entity.TeamManager;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  *
  * @author Marian Sulgan
  */
+@Repository
+@Transactional
 public class TeamManagerDaoImpl implements TeamManagerDao {
     
     @PersistenceContext
