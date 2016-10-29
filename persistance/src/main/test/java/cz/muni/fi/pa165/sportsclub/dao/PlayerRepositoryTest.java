a1065117714295de866d910f0396a12423ad922d
norberfabian <fabian.norbert@hotmail.sk>
2016-10-29 10:48:50 +0200

    Remove JUnit

--- persistance/src/main/test/java/cz/muni/fi/pa165/sportsclub/dao/PlayerRepositoryTest.java
+++ persistance/src/main/test/java/cz/muni/fi/pa165/sportsclub/dao/PlayerRepositoryTest.java
@@ -3,10 +3,8 @@ package cz.muni.fi.pa165.sportsclub.dao;
 import cz.muni.fi.pa165.sportsclub.EntityFactory;
 import cz.muni.fi.pa165.sportsclub.PersistenceSampleApplicationContext;
 import cz.muni.fi.pa165.sportsclub.entity.Player;
-import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestExecutionListeners;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
 import org.springframework.transaction.annotation.Transactional;
@@ -23,7 +21,6 @@ import javax.validation.ConstraintViolationException;
 
 @ContextConfiguration(classes = PersistenceSampleApplicationContext.class)
 @TestExecutionListeners(TransactionalTestExecutionListener.class)
-@RunWith(SpringJUnit4ClassRunner.class)
 @Transactional
 public class PlayerRepositoryTest extends AbstractTestNGSpringContextTests {
 
--- persistance/src/main/test/java/cz/muni/fi/pa165/sportsclub/dao/TeamRepositoryTest.java
+++ persistance/src/main/test/java/cz/muni/fi/pa165/sportsclub/dao/TeamRepositoryTest.java
@@ -5,15 +5,13 @@ import javax.inject.Inject;
 
 import cz.muni.fi.pa165.sportsclub.PersistenceSampleApplicationContext;
 import cz.muni.fi.pa165.sportsclub.entity.Team;
-import org.junit.Assert;
-import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestExecutionListeners;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
 import org.springframework.transaction.annotation.Transactional;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 /**
  * Created by norbert on 24.10.16.
@@ -21,7 +19,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 @ContextConfiguration(classes = PersistenceSampleApplicationContext.class)
 @TestExecutionListeners(TransactionalTestExecutionListener.class)
-@RunWith(SpringJUnit4ClassRunner.class)
 @Transactional
 public class TeamRepositoryTest extends AbstractTestNGSpringContextTests {
 
@@ -33,6 +30,6 @@ public class TeamRepositoryTest extends AbstractTestNGSpringContextTests {
         Team team = new Team();
         team.setName("Team");
         teamDao.create(team);
-        Assert.assertEquals("CreateTest Team failed. Id was not assigned", 1L, team.getId());
+        Assert.assertEquals(1L, team.getId());
     }
 }
