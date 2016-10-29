04210329fc07d644d85f95d5a55fc732c3a62b70
norberfabian <fabian.norbert@hotmail.sk>
2016-10-29 07:55:47 +0200

    Add OneToOne annotation to User.TeamManager

--- persistance/src/main/java/cz/muni/fi/pa165/sportsclub/entity/User.java
+++ persistance/src/main/java/cz/muni/fi/pa165/sportsclub/entity/User.java
@@ -5,11 +5,7 @@
  */
 package cz.muni.fi.pa165.sportsclub.entity;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -40,7 +36,8 @@ public class User {
     
     @NotNull
     private String role;
-    
+
+    @OneToOne
     private TeamManager teamManager;
 
     public long getId() {
