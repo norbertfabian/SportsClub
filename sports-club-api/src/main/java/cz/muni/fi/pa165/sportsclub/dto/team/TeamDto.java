b62d2a6d059f28befbe289814c39903d04a09a85
norberfabian <fabian.norbert@hotmail.sk>
2016-11-19 11:02:33 +0100

    Add sports-club-api module

---
+++ sports-club-api/src/main/java/cz/muni/fi/pa165/sportsclub/dto/team/TeamDto.java
@@ -0,0 +1,7 @@
+package cz.muni.fi.pa165.sportsclub.dto.team;
+
+/**
+ * Created by norbert on 19.11.16.
+ */
+public class TeamDto {
+}
---
+++ sports-club-api/src/main/java/cz/muni/fi/pa165/sportsclub/facade/TeamFacade.java
@@ -0,0 +1,7 @@
+package cz.muni.fi.pa165.sportsclub.facade;
+
+/**
+ * Created by norbert on 19.11.16.
+ */
+public interface TeamFacade {
+}
