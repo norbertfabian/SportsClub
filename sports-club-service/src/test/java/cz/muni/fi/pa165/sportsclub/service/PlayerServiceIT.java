663dd7f340d7f6658291401f3d1fd9a35086d87e
novaakpatrik <novaak.patrik@gmail.com>
2016-11-29 13:47:54 +0100

    Added test, which got lost somewhere in versioning.

--- sports-club-service/src/test/java/cz/muni/fi/pa165/sportsclub/service/PlayerServiceIT.java
+++ sports-club-service/src/test/java/cz/muni/fi/pa165/sportsclub/service/PlayerServiceIT.java
@@ -1,7 +1,73 @@
 package cz.muni.fi.pa165.sportsclub.service;
 
+import cz.muni.fi.pa165.sportsclub.EntityFactoryService;
+import cz.muni.fi.pa165.sportsclub.SpringContextConfiguration;
+import cz.muni.fi.pa165.sportsclub.dao.PlayerDao;
+import cz.muni.fi.pa165.sportsclub.entity.Player;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTransactionalTestNGSpringContextTests;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import javax.inject.Inject;
+import java.util.List;
+
 /**
  * @author Patrik Novak.
  */
-public class PlayerServiceIT {
+@ContextConfiguration(classes = SpringContextConfiguration.class)
+public class PlayerServiceIT extends AbstractTransactionalTestNGSpringContextTests {
+
+    @Inject
+    private PlayerService playerService;
+
+    @Inject
+    private PlayerDao playerDao;
+
+    private EntityFactoryService entityFactoryService = new EntityFactoryService();
+
+    @Test
+    public void findByIdIT() {
+        Player player = entityFactoryService.createPersistedPlayer(playerDao);
+        Assert.assertNotNull(playerService.findById(player.getId()));
+    }
+
+    @Test
+    void getAllIT() {
+        Player player1 = entityFactoryService.createPersistedPlayer("Player1", playerDao);
+        Player player2 = entityFactoryService.createPersistedPlayer("Player2", playerDao);
+
+        List<Player> result = playerDao.getAll();
+
+        Assert.assertNotNull(result);
+        Assert.assertEquals(2, result.size());
+        Assert.assertTrue(result.contains(player1));
+        Assert.assertTrue(result.contains(player2));
+    }
+
+    @Test
+    public void createPlayerIT() {
+        Player player = entityFactoryService.createPlayer();
+        playerService.createPlayer(player);
+        Assert.assertTrue(player.getId() > 0);
+        Assert.assertNotNull(playerDao.findById(player.getId()));
+    }
+
+    @Test
+    public void updatePlayerIT() {
+        Player player = entityFactoryService.createPersistedPlayer(playerDao);
+        player.setFirstName("updatedName");
+        Player result = playerService.updatePlayer(player);
+        Assert.assertNotNull(result);
+        Assert.assertEquals("updatedName", playerDao.findById(player.getId()).getFirstName());
+    }
+
+    @Test
+    public void removePlayerIT() {
+        Player playerToRemove = entityFactoryService.createPersistedPlayer(playerDao);
+        long id = playerToRemove.getId();
+        playerService.removePlayer(playerToRemove.getId());
+        Assert.assertNull(playerDao.findById(id));
+    }
 }
+
--- sports-club-service/src/test/java/cz/muni/fi/pa165/sportsclub/service/PlayerServiceTest.java
+++ sports-club-service/src/test/java/cz/muni/fi/pa165/sportsclub/service/PlayerServiceTest.java
@@ -1,7 +1,87 @@
 package cz.muni.fi.pa165.sportsclub.service;
 
+import cz.muni.fi.pa165.sportsclub.EntityFactoryService;
+import cz.muni.fi.pa165.sportsclub.dao.PlayerDao;
+import cz.muni.fi.pa165.sportsclub.entity.Player;
+import cz.muni.fi.pa165.sportsclub.service.impl.PlayerServiceImpl;
+import org.hibernate.service.spi.ServiceException;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author Patrik Novak.
  */
 public class PlayerServiceTest {
+
+    @Mock
+    private PlayerDao playerDao;
+
+    @InjectMocks
+    private PlayerService playerService = new PlayerServiceImpl();
+
+    private EntityFactoryService entityFactoryService = new EntityFactoryService();
+
+    @BeforeClass
+    public void setUpClass() throws ServiceException {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void findByIdPlayerTest() {
+        Mockito.when(playerDao.findById(Mockito.anyLong()))
+                .thenReturn(entityFactoryService.createPlayer());
+
+        Player result = playerService.findById(1L);
+
+        Mockito.verify(playerDao, Mockito.times(1)).findById(Mockito.anyLong());
+        Assert.assertNotNull(result);
+    }
+
+    @Test
+    public void getAllTest() {
+        List<Player> toReturn = new ArrayList<>();
+        toReturn.add(entityFactoryService.createPlayer());
+        Mockito.when(playerDao.getAll()).thenReturn(toReturn);
+
+        List<Player> result = playerService.getAll();
+
+        Mockito.verify(playerDao, Mockito.times(1)).getAll();
+        Assert.assertNotNull(result);
+        Assert.assertEquals(1, result.size());
+    }
+
+    @Test
+    public void createPlayerTest() {
+        playerService.createPlayer(entityFactoryService.createPlayer());
+
+        Mockito.verify(playerDao, Mockito.times(1)).create(Mockito.any());
+    }
+
+    @Test
+    public void updatePlayerTest() {
+        Player player = entityFactoryService.createPlayer();
+        Mockito.when(playerDao.update(Mockito.any())).thenReturn(player);
+
+        Player result = playerService.updatePlayer(player);
+
+        Mockito.verify(playerDao, Mockito.times(1)).update(Mockito.any());
+        Assert.assertEquals(player, result);
+    }
+
+    @Test
+    public void removePlayerTest() {
+        Player player = entityFactoryService.createPlayer();
+
+        playerService.removePlayer(player.getId());
+
+        Mockito.verify(playerDao, Mockito.times(1)).remove(Mockito.anyLong());
+    }
 }
