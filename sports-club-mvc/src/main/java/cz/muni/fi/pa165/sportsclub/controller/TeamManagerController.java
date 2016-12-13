ca035e18d74d0d9ae93eb303e64fe0d3a14fb977
MarianSulgan <marian.sulgan@gmail.com>
2016-12-14 00:18:44 +0100

    TeamManager controller, jsp, web sources added

---
+++ sports-club-mvc/src/main/java/cz/muni/fi/pa165/sportsclub/controller/TeamManagerController.java
@@ -0,0 +1,60 @@
+package cz.muni.fi.pa165.sportsclub.controller;
+
+import cz.muni.fi.pa165.sportsclub.dto.teamManager.TeamManagerDto;
+import cz.muni.fi.pa165.sportsclub.facade.TeamFacade;
+import cz.muni.fi.pa165.sportsclub.facade.TeamManagerFacade;
+import javax.inject.Inject;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ *
+ * @author Marian Sulgan
+ */
+@Controller
+@RequestMapping("/team-manager")
+public class TeamManagerController {
+    
+    @Inject
+    TeamManagerFacade tmFacade;
+    
+    @Inject
+    TeamFacade teamFacade;
+    
+    @RequestMapping(method = RequestMethod.POST)
+    public String createTeamManager(Model model) {
+        return null;
+    }
+    
+    
+    public String deleteTeamManager(Model model) {
+        return null;
+    }
+    
+    
+    public String updateTeamManager(Model model) {
+        return null;
+    }
+    
+    
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public String getTeamManager(@PathVariable("id") long id, Model model) {
+        TeamManagerDto tmDto = tmFacade.getTeamManager(id);
+        if (tmDto != null) {
+            model.addAttribute("teamManager", tmDto);
+            return "team-manager/detail";
+        } else {
+            return null;
+        }
+    }
+    
+    @RequestMapping(method = RequestMethod.GET)
+    public String getTeamManagers(Model model) {
+        model.addAttribute("teamManagers", tmFacade.getAllTeamManagers());
+        return "team-manager/list";
+    }
+    
+}
---
+++ sports-club-mvc/src/main/webapp/WEB-INF/jsp/team-manager/list.jsp
@@ -0,0 +1,9 @@
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@taglib prefix="t" tagdir="/WEB-INF/tags" %>
+
+<t:pagetemplate pageTitle="List of team managers">
+    <jsp:attribute name="content">
+      <h1>Welcome to list of managers</h1>
+      <p>List</p>
+    </jsp:attribute>
+</t:pagetemplate>
